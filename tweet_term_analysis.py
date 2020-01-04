affa20782872e6b0d78419b897e4e0fba1f931a3
shrutimdeka <39517789+shrutimdeka@users.noreply.github.com>
2020-01-04 12:05:40 +0530

    Updated comments
    Created positive and negative wordcloud. Data cleaning.

--- tweet_term_analysis.py
+++ tweet_term_analysis.py
@@ -6,26 +6,22 @@ Created on Fri Nov  8 13:23:43 2019
 """
 
 import string
-import tweepy  #move entire folder to Ananconda3/Lib/site-packages
+import tweepy 
 help(tweepy)
 import nltk
-import sys
-print(sys.path)
-sys.path.append("C:/Users/Shruti/AppData/Local/Programs/Python/Python37-32/Lib/site-packages")
-
 from wordcloud import WordCloud 
 from bs4 import BeautifulSoup as bs
 #Twitter API credentials
-consumer_key = "VX6UxbFfJiDa9P9xBZ3uaNIV5"
-consumer_secret = "UTi4c720q6HzOTjyp6k2CsaVUNzO6CHuFjLPUzrQ36yIExRYSF"
-access_key = "1183235731453403136-uSrpxEv7iAkuryoxYVXaWnL2NU7cEL"
-access_secret = "VW71fMl1RQFV5Esgsz1IWpZ6NptT7uH6wD91wDMcvJB9R"
+consumer_key = "########"
+consumer_secret = "########"
+access_key = "#######"
+access_secret = "##########"
 
-#function to read all tweets of a given user
+#function to read all tweets on a topic
 def read_tweets(search_term):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  #keys are confidential
+    auth.set_access_token(access_key, access_secret)  #use your own after applying for access 
+    api = tweepy.API(auth)  #twitter Api
     	
     # Collect tweets
     tweets = tweepy.Cursor(api.search,
@@ -39,12 +35,12 @@ def read_tweets(search_term):
         l = l + tweet.text
     #to csv
     with open(search_term+"_tweets.txt", "w", encoding="utf8") as obj:
-        obj.write(str(l))
+        obj.write(str(l))  #write the tweets into a file
     return tweets
 
-t= read_tweets("Modi")
+t= read_tweets("Modi") #topic = Modi
 
-#read from the txt
+#read from the txt file
 twit=""
 with open("Modi_tweets.txt", "r", encoding="utf8") as obj:
     twit = obj.read()
@@ -53,17 +49,21 @@ len(twit) #1 lakhs
 #clean data
 import re
 import pandas as pd
-whitelist = set('abcdefghijklmnopqrstuvwxyz ')  #the gap matters
-twit_string = ''.join(filter(whitelist.__contains__, twit.lower()))
-clean_tweets = re.sub("[^A-Za-z" "]+"," ", twit_string).lower()
-#must remove 'https?', bjp/s, pm/s, narendra, specialized words used in the context needs to be added to list
-clean_tweets = re.sub("modi?","", str(clean_tweets)) #hindutva, bhakt, hinduvta, guru, mahan,- add
-clean_tweets = re.sub("http?","", str(clean_tweets)) #ok
+whitelist = set('abcdefghijklmnopqrstuvwxyz ')  #the gap at the end matters
+twit_string = ''.join(filter(whitelist.__contains__, twit.lower()))  #only keep the whitelisted characters from tweets and join with a space
+clean_tweets = re.sub("[^A-Za-z" "]+"," ", twit_string).lower()  #all characters turned to lower case
+
+#Now...remove unneccesary words and meaningless terms
+#must remove 'https?', bjp/s, pm/s, narendra.
+#also, specialized words used in the context needs to be added to list
+clean_tweets = re.sub("modi?","", str(clean_tweets)) #hindutva, bhakt, hinduvta, guru, mahan,- add to list
+clean_tweets = re.sub("http?","", str(clean_tweets)) #cleaned
+
 #tokenize the words
 tokens = clean_tweets.split(" ")  #18k words
 token_no = len(tokens)-1
 
-#redundant words
+#redundant words removal
 words= []
 for i in range(0, token_no) :
     if len(tokens[i]) > 3 :
@@ -71,13 +71,13 @@ for i in range(0, token_no) :
 len(words) #12k
 
 #due to bad extraction/ partial tweets, meaningful words have merged forming a meaningless word. extract them
-#stopwords
+#use stopwords
 with open("C:/Users/Shruti/Downloads/Excelr-assignment/Ass11-Text_Mining/stop.txt", 'r') as file:
     stopwords = file.read()
 words_stop = [w for w in words if not w in stopwords]
 len(words_stop) #18k
 
-#remove obvious words like narendra, bjp..
+#remove obvious words(non-sentimental) like narendra, bjp, india..
 l = ['bjp', 'bjps', 'narendra', 'india', 'modi', 'modis', 'indias', 'indian', 'indians', 'raydalio', 'taseer', 'pokershash', 'aatish', 'bjpindia', 'bjpindian', 'govt']
 
 for k in words_stop:
@@ -87,20 +87,20 @@ for k in words_stop:
 import matplotlib.pyplot as plt
 
 #import wordcloud
-from wordcloud import WordCloud
-# Joinining all the reviews into single paragraph 
+# Joining all the reviews into single paragraph 
 string_modi = " ".join(words_stop) #tokens to single string
 fig = plt.figure(figsize = (50, 50))
 wordcloud_ip = WordCloud(
                       background_color='white',
                       width=4000, height=500).generate(string_modi)
 
-plt.imshow(wordcloud_ip) 
+plt.imshow(wordcloud_ip) #all wordcloud 
 
 #add positive words
 with open("C:/Users/Shruti/Downloads/Excelr-assignment/Ass11-Text_Mining/positive-words.txt", 'r') as obj:
     pos= obj.read().split("\n")
 pos_review = [word for word in words_stop if word in pos ] #take tokenized version, not string
+
 # Joinining all the reviews into single paragraph 
 string_modi = " ".join(pos_review) #tokens to single string
 fig = plt.figure(figsize = (50, 50))
@@ -108,7 +108,7 @@ wordcloud_ip = WordCloud(
                       background_color='white',
                       width=4000, height=500).generate(string_modi)
 
-plt.imshow(wordcloud_ip) 
+plt.imshow(wordcloud_ip) #positive wordcloud
 
 #negative
 with open("C:/Users/Shruti/Downloads/Excelr-assignment/Ass11-Text_Mining/negative-words.txt", 'r') as obj:
@@ -124,4 +124,4 @@ wordcloud_ip = WordCloud(
                       background_color='white',
                       width=4000, height=500).generate(string_modi)
 
-plt.imshow(wordcloud_ip) 
+plt.imshow(wordcloud_ip) #negative wordcloud
