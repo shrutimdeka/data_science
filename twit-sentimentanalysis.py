1fea202da9d48f590a93b44fb68bbd90ceb369e0
shrutimdeka <39517789+shrutimdeka@users.noreply.github.com>
2020-01-04 11:40:10 +0530

    Updated comments to become more readable

--- twit-sentimentanalysis.py
+++ twit-sentimentanalysis.py
@@ -5,25 +5,17 @@ Created on Sun Nov  3 17:31:06 2019
 @author: Shruti
 """
 import string
-import tweepy  #move entire folder to Ananconda3/Lib/site-packages
-help(tweepy)
 import nltk
-import sys
-print(sys.path)
-sys.path.insert(0, 'C:\\Users\\Shruti\\AppData\\Local\\Programs\\Python\\Python37-32\\Lib\\site-packages') #temporary path
-
-import sys
-sys.path.append("C:/Users/Shruti/AppData/Local/Programs/Python/Python37-32/Lib/site-packages")
 
 import tweepy
 from wordcloud import WordCloud 
 import re
 from bs4 import BeautifulSoup as bs
 #Twitter API credentials
-consumer_key = "VX6UxbFfJiDa9P9xBZ3uaNIV5"
-consumer_secret = "UTi4c720q6HzOTjyp6k2CsaVUNzO6CHuFjLPUzrQ36yIExRYSF"
-access_key = "1183235731453403136-uSrpxEv7iAkuryoxYVXaWnL2NU7cEL"
-access_secret = "VW71fMl1RQFV5Esgsz1IWpZ6NptT7uH6wD91wDMcvJB9R"
+consumer_key = "##########"
+consumer_secret = "#########"
+access_key = "########"
+access_secret = "#######"
 
 #function to read all tweets of a given user
 def read_tweets(user_name):
@@ -34,24 +26,26 @@ def read_tweets(user_name):
     new_tweets = api.user_timeline(screen_name = user_name,count=200)
     alltweets.extend(new_tweets)  #all tweets in one
     
-    oldest = alltweets[-1].id - 1 #last of all tweets = oldest
+    oldest = alltweets[-1].id - 1 #last of all tweets ie, oldest ones
     while 0<len(new_tweets)<2000:
         new_tweets = api.user_timeline(screen_name = user_name,
-                                       count=200, 
-                                       max_id=oldest
+                                       count=200, #200 pages
+                                       max_id=oldest   #not more than the last(oldest) tweet
                                        )
         #save most recent tweets
         alltweets.extend(new_tweets)
         #update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-        print ("...%s tweets downloaded so far" % (len(alltweets)))          #monitor
+        print ("...%s tweets downloaded so far" % (len(alltweets)))          #monitor the process
  
     outtweets = [[tweet.created_at,tweet.entities["hashtags"],tweet.entities["user_mentions"],tweet.favorite_count,
                   tweet.geo,tweet.id_str,tweet.lang,tweet.place,tweet.retweet_count,tweet.retweeted,tweet.source,tweet.text,
                   tweet._json["user"]["location"],tweet._json["user"]["name"],tweet._json["user"]["time_zone"],
-                  tweet._json["user"]["utc_offset"]] for tweet in alltweets]
+                  tweet._json["user"]["utc_offset"]] for tweet in alltweets] #characteristics of each tweet like location, time, hashtags
     
     import pandas as pd
+    #create dataframe out of the tweets
+    #each characteristic/feature of tweets makes a column
     tweets_df = pd.DataFrame(columns = ["time","hashtags","user_mentions","favorite_count",
                                     "geo","id_str","lang","place","retweet_count","retweeted","source",
                                     "text","location","name","time_zone","utc_offset"])
@@ -75,4 +69,5 @@ def read_tweets(user_name):
     return tweets_df
 
 
-user_acc = read_tweets("realDonaldTrump")
+user_acc = read_tweets("realDonaldTrump") #username supplied to the function 'read_tweets' to extract tweets
+
